2fbaace862600184acf4ed7e1e05e463e1d0cc71
moonditch <156866717+moonditch@users.noreply.github.com>
2024-01-24 17:45:00 +0800

    Update ItemTooltips.cs

--- Items/ItemTooltips.cs
+++ Items/ItemTooltips.cs
@@ -12,8 +12,8 @@ namespace ThoriumModzhcn.Items
         {
             foreach (TooltipLine tooltipLine in tooltips)
             {
-                tooltipLine.Text = tooltipLine.Text.Replace("-Dedicated Content-", "-专用内容-");
-                tooltipLine.Text = tooltipLine.Text.Replace("-Crossover Content-", "-Crossover内容-");
+                tooltipLine.Text = tooltipLine.Text.Replace("-Dedicated Content-", "-Dedicated Content1-");
+                tooltipLine.Text = tooltipLine.Text.Replace("-Crossover Content-", "-Crossover Content1-");
                 tooltipLine.Text = tooltipLine.Text.Replace("Trinket", "杂货");
                 tooltipLine.Text = tooltipLine.Text.Replace("No Random Crit Chance", "没有概率暴击");
                 tooltipLine.Text = tooltipLine.Text.Replace("combo power cost", "combo power cost1");
