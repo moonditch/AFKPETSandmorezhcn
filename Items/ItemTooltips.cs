6523b763d2f369cc4c921133e80e4b9a034707ce
moonditch <156866717+moonditch@users.noreply.github.com>
2024-01-25 12:11:30 +0800

    Update ItemTooltips.cs

--- Items/ItemTooltips.cs
+++ Items/ItemTooltips.cs
@@ -12,11 +12,13 @@ namespace ThoriumModzhcn.Items
         {
             foreach (TooltipLine tooltipLine in tooltips)
             {
-                tooltipLine.Text = tooltipLine.Text.Replace("-Dedicated Content-", "-Dedicated Content1-");
-                tooltipLine.Text = tooltipLine.Text.Replace("-Crossover Content-", "-Crossover Content1-");
+                tooltipLine.Text = tooltipLine.Text.Replace("-Dedicated Content-", "-Dedicated Content-");
+                tooltipLine.Text = tooltipLine.Text.Replace("-Crossover Content-", "-CrossoverContent-");
                 tooltipLine.Text = tooltipLine.Text.Replace("Trinket", "杂货");
-                tooltipLine.Text = tooltipLine.Text.Replace("No Random Crit Chance", "没有概率暴击");
+                tooltipLine.Text = tooltipLine.Text.Replace("No Random Crit Chance", "无暴击率");
                 tooltipLine.Text = tooltipLine.Text.Replace("combo power cost", "combo power cost1");
+                tooltipLine.Text = tooltipLine.Text.Replace("A divine relic which made from great harpy bars", "这是由鹰身女妖制成的圣物");
+                tooltipLine.Text = tooltipLine.Text.Replace("Hold Shift while hovering on item to see lil wiki obtain entry", "鼠标指针放在物品上并按住Shift键，可以看到lil wiki上面的获取方式");
             }
         }
     }
