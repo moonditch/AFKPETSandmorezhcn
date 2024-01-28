c54d163a1acf0ed6e064d0088781ac842de3c3fa
urgiv <134754102+urgiv@users.noreply.github.com>
2024-01-28 18:00:43 +0800

    召唤武器、盗贼武器转化提示、句号

--- Items/ItemTooltips.cs
+++ Items/ItemTooltips.cs
@@ -22,10 +22,14 @@ namespace AFKPETSandmorezhcn.Items
 				sb.Replace("transformation summon ", "变身");
 				sb.Replace("No Random Crit Chance", "无暴击率");
 				sb.Replace(" combo power cost", "点连击能量消耗");
+
 				sb.Replace("A divine relic which made from great harpy bars", "这是由鹰身女妖制成的圣物");
+				sb.Replace("Taking heart while having full hp spawn dolls that attack close enemies\nIf player have ability to flight they also gain that ability",
+							"满血状态获得心时，生成攻击敌人的玩偶\n玩偶会复制玩家的攻击手段");
 
 				sb.Replace("Hold Shift while hovering on item to see lil wiki obtain entry", "光标放在物品上并按住Shift键，查看 Wiki 上的获取方式");
 				sb.Replace("Hold Shift while hovering on item to see lil wiki secret + obtain entry", "光标放在物品上并按住Shift键，查看 Wiki 上的获取方式");
+				sb.Replace("Hold Shift while hovering on item to learn more about potted pals", "光标放在物品上并按住Shift键，查看 Wiki 上的详细说明");
 				sb.Replace("Hold Shift while hovering on item to see lil wiki blessing ability entry", "光标放在物品上并按住Shift键，查看 Wiki 上的详细说明");
 				sb.Replace("Hold Shift while hovering on item to see lil wiki different poop types entry", "光标放在物品上并按住Shift键，查看 Wiki 上的详细说明");
 				sb.Replace("Hold Shift while hovering on item to see lil wiki all tiny crew ability entry", "光标放在物品上并按住Shift键，查看 Wiki 上的详细说明");
