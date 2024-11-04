a3f8566b74e725a8c2967d46950271bf4e0fe4fc
urgiv <134754102+urgiv@users.noreply.github.com>
2024-11-04 10:04:15 +0800

    适配 v9.9.9.78

--- AFKPETSandmorezhcn.cs
+++ AFKPETSandmorezhcn.cs
@@ -11,7 +11,7 @@ namespace AFKPETSandmorezhcn
 	{
 		public override void OnEnterWorld()
 		{
-			Main.NewText("汉化更新，添加了NPC对话的汉化.\nAFK模组更新 v9.9.9.77 ，死灵傀儡法师和胎儿博士战斗改进.", Color.Orange);
+			Main.NewText("AFK模组更新 v9.9.9.78 ，物品重做和战斗改进，具体参照模组创意工坊的更新日志.", Color.Orange);
 		}
 	}
 }
